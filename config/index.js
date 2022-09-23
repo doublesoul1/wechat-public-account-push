4f82aa8bf9443e13aaeb9d365d61317cb928b315
doublesoul1 <113874593+doublesoul1@users.noreply.github.com>
2022-09-23 13:17:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oi5AX6MPiFar0WGHoYiGrwS2vu8o",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "PTMGyzZtOZChiK_jiOZ-O85VYRpIpE6DjhcfNMG5jhA",
+      useTemplateId: "vq7Q3s17O3aSnnmcOjrwollE5Y3REx2dwbWVYFGzm30",
       // 所在省份
       province: "陕西",
       // 所在城市
