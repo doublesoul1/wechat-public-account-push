8d07cc63eacdd95c2d4c098e93597c204356d68b
doublesoul1 <113874593+doublesoul1@users.noreply.github.com>
2022-09-23 12:56:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,8 +60,7 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "xx",
-      \n,
+      name: "媳妇儿",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oi5AX6MPiFar0WGHoYiGrwS2vu8o",
       // 你想对他发送的模板消息的模板ID
