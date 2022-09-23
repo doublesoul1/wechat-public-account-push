1da35cae8d604917dd924a77b97b91726b4f818c
doublesoul1 <113874593+doublesoul1@users.noreply.github.com>
2022-09-23 10:50:02 +0800

    Update index.js

--- 配置/index.js
+++ 配置/index.js
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oi5AX6MPiFar0WGHoYiGrwS2vu8o",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "vq7Q3s17O3aSnnmcOjrwollE5Y3REx2dwbWVYFGzm30",
       // 所在省份
       province: "广东",
       // 所在城市
