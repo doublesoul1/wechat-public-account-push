b4a8417c95d080b83a1bd43d88b33502b3999f8b
doublesoul1 <113874593+doublesoul1@users.noreply.github.com>
2022-09-23 11:46:29 +0800

    Update and rename config/index.js to 配置/index.js

--- 配置/index.js
+++ 配置/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx83bf8dcbe561106a",
+  APP_ID: "",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "9ded2bfac290e51edb0b3994aa09e371",
+  APP_SECRET: "",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
